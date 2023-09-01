4b0f5418cbb6f46585b617c84556d890221afc20
Namratha <namrathapmd@gmail.com>
2023-09-01 18:03:48 -0500

    cleaned up the code

--- homework.py
+++ homework.py
@@ -1,19 +1,43 @@
 class Line:
     def __init__(self, coor1, coor2):
-        self.coorx1, self.coory1 = coor1
-        self.coorx2, self.coory2 = coor2
-        self.coorxdiff = self.coorx2-self.coorx1
-        self.coorydiff = self.coory2-self.coory1
+        self.x1, self.y1 = coor1
+        self.x2, self.y2 = coor2
+        self.xdiff = self.x2-self.x1
+        self.ydiff = self.y2-self.y1
 
     def distance(self):
-        print(((self.coorxdiff**2)+(self.coorydiff**2))**(1/2))
+        return ((self.xdiff ** 2) + (self.ydiff ** 2)) ** (1/2)
 
     def slope(self):
-        print(self.coorydiff/self.coorxdiff)
+        return self.ydiff / self.xdiff
 
 
 coordinate1 = (3, 2)
 coordinate2 = (8, 10)
 li = Line(coordinate1, coordinate2)
-li.distance()
-li.slope()
+print(li.distance())
+print(li.slope())
+
+
+class Cylinder:
+
+    pi = 3.14
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        vol = self.pi * (self.radius ** 2) * self.height
+        return vol
+
+    def surface_area(self):
+        area = (2 * self.pi * self.radius * self.height) + \
+            (2 * self.pi * self.radius ** 2)
+        return area
+
+
+cylin = Cylinder(2, 3)
+
+print(cylin.volume())
+print(cylin.surface_area())
